e003b70c902bfe5cbc35ad7557b1d89b2649daa2
KennethNhunduru <133786329+KennethNhunduru@users.noreply.github.com>
2024-08-31 02:59:14 +0100

    Add project files.

---
+++ MethodClassAssignment/MathMethod.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodClassAssignment
+{
+    class MathMethod
+    {
+        //declaring the two integers to be used as parameters for the void method. 
+        public int integer1;
+        public int integer2;
+
+        //Creating the void method that takes two integers as parameters.
+        public void MyMethod(int integer1, int integer2)
+        {
+            //Math operation on the first integer
+            //The VOID method does not return any value from the math operation
+
+            _ = integer1 + 8; //No need to declare variable for the result since method does not return an output.            
+
+            //Displaying the second integer to the screen or Console window.
+
+            Console.WriteLine("The second integer is: " + integer2);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ MethodClassAssignment/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodClassAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //INSTANTIATION OF THE MATHMETHOD CLASS
+            MathMethod MyMathMethod = new MathMethod();
+
+            //assigning values to the variables
+            MyMathMethod.integer1 = 2;
+            MyMathMethod.integer2 = 12;
+
+            //Calling the method in the class by passing in two numbers.
+            /*Note the arguments need to be in the same order as specified in the method declaration 
+              to be identified correctly for processing*/
+
+            MyMathMethod.MyMethod(2, 12);
+
+            //Calling the method in the class, specifying the parameters by name.
+            //In this case the arguments are sent with the key: value syntax.
+            //That way, the order of the arguments does not matter:
+
+            MyMathMethod.MyMethod(integer2:12, integer1:2);
+
+            //although the order in which the arguments are passed is reversed, the outcome is the same.
+        }
+    }
+}
